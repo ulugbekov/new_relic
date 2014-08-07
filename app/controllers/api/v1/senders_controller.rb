aa6acb5bdb13b1b3d63e9105ac96e96b55ece166
Alisher Ulugbekov <ulugbekov@Alishers-iMac.local>
2014-08-06 18:08:45 -0700

    Post Api creation and Email Validation

--- app/controllers/api/v1/senders_controller.rb
+++ app/controllers/api/v1/senders_controller.rb
@@ -3,7 +3,30 @@ class Api::V1::SendersController < ApplicationController
   respond_to :json
 
   def create
-  	
+  	respond_to do |format|
+  		if valid_email?(checked_params)
+  			
+  			format.json { render json: {success: true} }
+  		else
+  			format.json { render json: {success: false} }
+  		end
+  	end
   end
-  
+
+  private
+
+  def checked_params
+  	params.permit :to, :sender, :body
+  end
+
+  def valid_email?(data)
+  	email_regex = /<(([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+)>/
+  	to = data[:to]
+  	if to && m = to.match(email_regex)
+  		data[:email] = m[1]
+  		return true
+  	else
+  		return false
+  	end
+  end
 end
